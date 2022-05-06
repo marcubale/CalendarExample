730f923cd2177f0a52846d15a2564ccd45968f1c
bale_marcu <balemarcu23@gmail.com>
2022-05-06 20:47:49 +0300

    first commit

---
+++ CalendarExample/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  CalendarExample
+//
+//  Created by Marcu Bale on 06.05.2022.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell{
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
